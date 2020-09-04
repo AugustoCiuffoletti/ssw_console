b69256d2e961b709b66526caa8859da5736d9e9f
AugustoCiuffoletti <augusto@di.unipi.it>
2020-09-04 18:44:55 +0200

    1 file modified

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+console.log("Primo");
+console.log("Secondo");
+console.log("Terzo");
